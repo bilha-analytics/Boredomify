6ceadf1e930bb25a9394e8c3abd7cc9d5ae932a7
Bilha <32512727+bilha-analytics@users.noreply.github.com>
2019-11-26 20:22:22 +0300

    android app add v0.1

---
+++ BoredomifyApp/app/src/main/java/com/ba/streamreader/HomeMenuActivity.kt
@@ -0,0 +1,165 @@
+package com.ba.streamreader
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_home_menu.*
+import java.io.Serializable
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
+class HomeMenuActivity : AppCompatActivity() {
+
+    companion object{
+        val STR_XKCD = "XKCD"
+        val STR_MAKEUP = "Makeup"
+        val STR_QUOTES = "Quotes"
+        val STR_BOOK_COVERS = "Book Covers"
+        val STR_CAT_FACTS  = "Cat Facts"
+        val STR_BORED_ACTIVITIES = "Bored?"
+        val STR_JOKES = "Joke-a-Jokes!"
+        val STR_RICK_MORTY = "Rick and Morty"
+        val STR_TRIVIA = "Trivia"
+        val STR_NUMBERS = "Number Facts"
+    }
+
+    data class MenuItem(
+        val name:String,
+        val description: String,
+        val icon: Int,
+        var url:String,
+        val hasImage: Boolean) : Serializable {
+
+        init { updateRandomURL()  }
+
+        fun updateRandomURL(){
+            if( name == STR_XKCD ){
+                url = "http://xkcd.com/${Random.nextInt(2226)}/info.0.json"
+            }else if( name == STR_BOOK_COVERS){
+                url = "http://covers.openlibrary.org/b/ID/${Random.nextLong(9099999)}-L.jpg"
+            }else if( name == STR_RICK_MORTY){
+                url = "https://rickandmortyapi.com/api/character/${Random.nextInt(493)}"
+            }else if (name == STR_NUMBERS){
+                val opts = listOf<String>("math", "trivia", "year", "date")
+                url = "http://numbersapi.com/random/${opts[Random.nextInt(opts.size - 1) ]}?json"
+            }
+
+        }
+
+    }
+
+    class MenuItemAdapter (
+        private var context: Context,
+        private var listData : ArrayList<MenuItem>): BaseAdapter(  ) {
+
+        private class ItemLayout(row: View?){
+            var name : TextView? = null
+            var description : TextView? = null
+            var icon : ImageView? = null
+
+            init {
+                this.name = row?.findViewById<TextView>(R.id.txt_menuTitle)
+                this.description = row?.findViewById<TextView>(R.id.txt_menuDescription)
+                this.icon = row?.findViewById<ImageView>(R.id.img_menuIcon)
+            }
+
+        }
+
+
+        override fun getCount(): Int {
+            return this.listData.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return this.listData[position]
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+            lateinit var view: View
+            lateinit var itemLayout : ItemLayout
+
+            if( convertView == null ){
+                val inflater = context.getSystemService( Context.LAYOUT_INFLATER_SERVICE ) as LayoutInflater
+                view = inflater.inflate(R.layout.liat_item_layout, null)
+                itemLayout = ItemLayout( view )
+                view?.tag = itemLayout
+
+            }else{
+                view = convertView
+                itemLayout = view.tag as ItemLayout
+            }
+
+            val item = listData[position]
+            itemLayout.name?.text = item.name
+            itemLayout.description?.text = item.description
+            itemLayout.icon?.setImageResource( item.icon )
+
+            return view
+        }
+
+    }
+
+
+
+//    private val homeMenu = listOf<String>("XKCD", "Makeup", "Quotes")
+
+    private val homeMenu: ArrayList<MenuItem> = ArrayList<MenuItem>()
+
+
+    private fun generateMenuListData() {
+        homeMenu.add(MenuItem(STR_XKCD, "Daily XKCD comic", R.drawable.xkcd, "http://xkcd.com/info.0.json", true ))
+        homeMenu.add(MenuItem(STR_MAKEUP, "Major makeup brands and their products", R.drawable.makeup, "https://makeup-api.herokuapp.com/api/v1/products.json", true) )
+        homeMenu.add(
+            MenuItem(
+                STR_QUOTES,
+                "Random common, inspirational, silly quotes",
+                R.drawable.quotes,
+                "https://quote-garden.herokuapp.com/quotes/random",
+                false
+            )
+        )
+        homeMenu.add( MenuItem(STR_BOOK_COVERS, "Random book covers discovery", R.drawable.books, "http://covers.openlibrary.org/b/ID/1-M.jpg", true))
+        homeMenu.add( MenuItem(STR_BORED_ACTIVITIES, "Are you bored? Try some of these activities", R.drawable.bored, "http://www.boredapi.com/api/activity/", false))
+        homeMenu.add( MenuItem(STR_CAT_FACTS, "Random infor about cats", R.drawable.cat_facts, "https://cat-fact.herokuapp.com/facts" , false ))
+        homeMenu.add( MenuItem(STR_JOKES, "Programming and random jokes", R.drawable.jokes, "https://official-joke-api.appspot.com/random_joke", false))
+        homeMenu.add( MenuItem(STR_RICK_MORTY, "Information about characters in Rick and Morty series", R.drawable.rick_morty, "https://rickandmortyapi.com/api/character/1", true))
+        homeMenu.add( MenuItem(STR_TRIVIA, "Open Trivia database", R.drawable.trivia, "https://opentdb.com/api.php?amount=1", false))
+        homeMenu.add( MenuItem(STR_NUMBERS, "Facts about numbers @ math, year, trivia", R.drawable.numbers, "http://numbersapi.com/random/year?json", false))
+
+    }
+
+//    private lateinit var adapter_homeMenu : ArrayAdapter<String>
+    private lateinit var adapter_homeMenu : MenuItemAdapter
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_menu)
+
+
+//        adapter_homeMenu = ArrayAdapter(this, android.R.layout.simple_list_item_1, homeMenu)
+        generateMenuListData()
+        adapter_homeMenu = MenuItemAdapter(this, homeMenu)
+
+        ls_homeMenu.adapter = adapter_homeMenu
+
+        ls_homeMenu.setOnItemClickListener(){ _, _, index, _ ->
+//            Toast.makeText(this, "Selected ${homeMenu[index]}", Toast.LENGTH_SHORT).show()
+            val intented = Intent( this, ItemViewActivity::class.java)
+            intented.putExtra("menuItem", homeMenu[index] )
+            startActivity( intented )
+        }
+    }
+}
---
+++ BoredomifyApp/app/src/main/java/com/ba/streamreader/ItemViewActivity.kt
@@ -0,0 +1,241 @@
+package com.ba.streamreader
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.widget.ProgressBar
+import androidx.core.text.HtmlCompat
+import com.koushikdutta.ion.Ion
+import com.skyfishjy.library.RippleBackground
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_item_view.*
+import kotlinx.android.synthetic.main.image_item_layout.view.*
+import kotlinx.android.synthetic.main.text_item_layout.view.*
+import org.json.JSONObject
+import java.io.Serializable
+import kotlin.random.Random
+
+class ItemViewActivity : AppCompatActivity() {
+
+//    private val textColors = listOf( "#99ff00", "#cc3399", "#ffff33", "#66ccff", "#cc6600")
+    private val textColors = listOf( Color.MAGENTA, Color.YELLOW, Color.CYAN, Color.GREEN, Color.BLUE)
+
+    data class ContentItem(
+        val hasImage: Boolean,
+        val title: String,
+        val subText:String,
+        val content:String): Serializable
+
+    private  lateinit var currentMenuItem : HomeMenuActivity.MenuItem
+
+    private lateinit var currentContentItem: ContentItem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_view)
+
+        if( intent != null ){
+            this.currentMenuItem = intent.getSerializableExtra("menuItem") as HomeMenuActivity.MenuItem
+            fetchAnother()
+        }
+    }
+
+    private fun prepDisplay(){
+        if( currentMenuItem != null){
+            txt_descriptionTitle.text = currentMenuItem.name + " <<< Loading... "
+            txt_descriptionDetails.text = currentMenuItem.description
+            lay_itemBox.removeAllViews()
+            val pb = findViewById<ProgressBar>( R.id.pb_progressBar)
+            if( pb != null) lay_itemBox.addView( pb )
+        }
+    }
+
+    fun fetchAnother(){
+        // show progress bar
+        prepDisplay()
+
+        // Dirty blocking fetch
+        if( currentMenuItem.name == HomeMenuActivity.STR_MAKEUP){
+            doProcess(getRandomMakeupItem())
+        }else if( currentMenuItem.name == HomeMenuActivity.STR_CAT_FACTS) {
+            //doProcess(getRandomCatFactItem())
+            Ion.with(this)
+                .load( "https://aws.random.cat/meow" ) // random cat images
+                .asString()
+                .setCallback{ e, res ->
+                    val jobj = JSONObject( getRandomCatFactItem() )
+                    jobj.put("img", JSONObject(res).getString("file"))
+                    doProcess( jobj.toString() )
+                }
+        } else if(currentMenuItem.name == HomeMenuActivity.STR_BOOK_COVERS){
+            currentMenuItem.updateRandomURL()
+            val jobj = JSONObject()
+            jobj.put("img", currentMenuItem.url )
+            doProcess( jobj.toString() )
+
+        } else {
+            currentMenuItem.updateRandomURL()
+
+            Ion.with(this)
+                .load(currentMenuItem.url)
+                .asString()
+                .setCallback{ e, res ->
+                    doProcess(res)
+                }
+        }
+    }
+
+    private fun getRandomMakeupItem() : String {
+        val json = JSONObject( "${MainActivity.makeupList_JSONObject}" )
+        val ls = json.getJSONArray("obj")
+        val res = ls.getString( Random.nextInt( ls.length() - 1 ) )
+        return  res
+    }
+
+
+    private fun getRandomCatFactItem() : String {
+        val json = JSONObject( "${MainActivity.catfactsList_JSONObject}" )
+        val ls = json.getJSONArray("all")
+        val res = ls.getString( Random.nextInt( ls.length() - 1 ) )
+        return  res
+    }
+
+    fun doProcess(result:String) {
+        if (result == null) return
+
+        try {
+            var json = JSONObject(result)
+
+            var isImaged: Boolean = false
+            var dtitle: String = ""
+            var subtext: String = ""
+            var content: String = ""
+
+            if (currentMenuItem.name == HomeMenuActivity.STR_XKCD) {
+                isImaged = true
+                dtitle = "" + json.getString("day") + "-" +
+                        json.getString("month") + "-" +
+                        json.getString("year") + ", " +
+                        json.getString("safe_title")
+                subtext = json.getString("alt")
+                content = json.getString("img")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_MAKEUP) {
+                isImaged = true
+                dtitle = json.getString("name") + " by " + json.getString("brand")
+                subtext = json.getString("description")
+                content = json.getString("image_link")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_QUOTES) {
+                isImaged = false
+                dtitle = json.getString("quoteAuthor")
+                content = json.getString("quoteText")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_NUMBERS) {
+                isImaged = false
+                dtitle = json.getString("type").capitalize() + ":\t" + json.getString("number")
+//                content = "" + json.getString("text")
+                content = Html.fromHtml(json.getString("text")).toString()
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_JOKES) {
+                isImaged = false
+                dtitle = json.getString("type")
+                content = "" + json.getString("setup") + "\n\n" + json.getString("punchline")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_TRIVIA) {
+                isImaged = false
+                val res = json.getJSONArray("results").get(0)
+                if (res != null) {
+                    json = JSONObject(res.toString())
+                    dtitle = json.getString("category")
+                    subtext = "Difficulty:\t" + json.getString("difficulty") +
+                            "\nAnswer:\t" + json.getString("correct_answer")
+                    content = Html.fromHtml(json.getString("question")).toString()
+                }
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_CAT_FACTS) {
+                isImaged = true
+                dtitle = "Random cat + Random fact" //json.getString( "type")
+                subtext = json.getString("text")
+                content = json.getString("img")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_BOOK_COVERS) {
+                isImaged = true
+                dtitle = "Random book covers" //json.getString( "type")
+                content = json.getString("img")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_BORED_ACTIVITIES) {
+                isImaged = false
+                dtitle = json.getString("type").capitalize()
+                subtext = "Can be done by " + json.getString("participants") + " person(s)." +
+                        "\nAccessibility: " + json.getString("accessibility") +
+                        "\nPrice rank: " + json.getString("price") +
+                        "\nLink: " + json.getString("link")
+                content = json.getString("activity")
+
+            } else if (currentMenuItem.name == HomeMenuActivity.STR_RICK_MORTY) {
+                isImaged = true
+                dtitle = json.getString("name")
+
+                val origin = json.getJSONObject("origin")
+                val home = json.getJSONObject("location")
+                content = json.getString("image")
+
+                subtext =
+                    json.getString("species") + ", " + json.getString("gender") +
+                            "\nStatus:\t " + json.getString("status") +
+                            "\nOrigin:\t" + origin.getString("name") +
+                            "\nResides:\t" + home.getString("name")
+
+            }
+
+            displayContent(isImaged, dtitle, subtext, content)
+        }catch (e:Exception){
+
+        }
+    }
+
+    private fun displayContent(isImaged:Boolean, dtitle:String, subtext: String, content: String){
+
+        currentContentItem = ContentItem(isImaged, dtitle, subtext, content)
+
+        lateinit var view : View
+
+        if( currentContentItem != null) {
+            txt_descriptionTitle.text = currentContentItem.title
+            txt_descriptionDetails.text = currentContentItem.subText
+
+            lay_itemBox.removeAllViews()
+            if (currentContentItem.hasImage) {
+
+                view = layoutInflater.inflate(R.layout.image_item_layout, null)
+//                view.img_itemImage.setImageResource(currentMenuItem.icon)
+                Picasso.get( )
+                    .load( currentContentItem.content )
+                    .resize( 1200, 0 )
+                    .into( view.img_itemImage )
+
+
+            } else {
+                view = layoutInflater.inflate(R.layout.text_item_layout, null)
+                view.txt_itemText.text = currentContentItem.content
+                view.txt_itemText.setTextColor( textColors[ Random.nextInt( textColors.size - 1) ])
+            }
+
+            lay_itemBox.addView( view )
+        }
+
+//        findViewById<RippleBackground>(R.id.rippler)?.startRippleAnimation()
+    }
+
+    fun onClickListener(v:View){
+        fetchAnother()
+    }
+
+
+    fun onZoomImage(v:View){
+
+    }
+}
